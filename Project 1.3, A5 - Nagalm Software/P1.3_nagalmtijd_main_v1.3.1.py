16b12abcc2e3752e8f0597d053bfe7481f0dc8be
LarsCD <larscdijkgraaf@gmail.com>
2023-03-27 20:19:47 +0200

    v1.3.1 - Commenting done

--- Project 1.3, A5 - Nagalm Software/P1.3_nagalmtijd_main_v1.3.1.py
+++ Project 1.3, A5 - Nagalm Software/P1.3_nagalmtijd_main_v1.3.1.py
@@ -1,4 +1,7 @@
-# Project 1.3 - Groep A5 - Nagalmtijd Software v0.1
+# Project 1.3 - Groep A5 - Nagalmtijd Software v1.3.1
+# Contributors: Lars Dijkgraaf, Kaylee Pond
+# Version release date: 27/3/2023
+
 import time
 import numpy as np
 import scipy.ndimage
@@ -10,12 +13,11 @@ from scipy.optimize import curve_fit
 import datetime
 
 
-#  de 'main' class houdt alle basis structuur van de code, zoals variabelen en de 'main_loop'
+# de 'main' class houdt alle basis structuur van de code, zoals variabelen en de 'main_loop'
 class Main:
     def __init__(self):
         self.run_program = True
         self.data_man = Data_management()
-        # TODO: add variables
 
 
     # 'main_loop' is de hoofd-loop die het programma laat draaien
@@ -23,68 +25,69 @@ class Main:
         # zolang 'self.run_program' True is, blijft 'main_loop' draaien
         duration = 0
         i = 1
-        valid1 = False
-        while valid1 != True:
+        save_data_question = ''
+        valid_duration = False
+        while valid_duration != True:
             try:
+                # verkrijg de tijdsduur voor elke meting
                 duration = float(input('Voor tijdsduur per meting in (seconde): '))
             except:
-                valid1 = False
+                valid_duration = False
             else:
-                valid1 = True
+                valid_duration = True
+
+        # Dit is de loop voor het programma:
         while self.run_program == True:
             try:
                 mic = Microphone()
-                RT60, raw_recording = mic.perform_recording(duration)
+
+                # doe de opnamen en bereken de RT60:
+                RT60 = mic.perform_recording(duration)
+
+                # vraag of de meting moet worden opgeslagen worden
                 save_data_question = input(f'Save meting? (y/..) (RT60: {RT60})s: ')
             except Exception as e:
                 print(f'\n!ERROR: RECORDING ERROR\n{e}')
             if save_data_question == 'y':
                 try:
+                    # verkrijg informatie voor het opslaan van de data
                     stoel = input('Voer stoel #nr in (naam): ')
                     x = input('x: ')
                     y = input('y: ')
                     z = input('z: ')
                     time_now = datetime.datetime.now()
-                    data = [f'Meting {i}', stoel, time_now, RT60, raw_recording, x, y, z]
+
+                    # doe alle data in een list, zodat het makkelijk in een .csv file kan worden opgeslagen
+                    data = [f'Meting {i}', stoel, time_now, RT60, x, y, z]
+
+                    # sla de data op in een .csv file
                     self.data_man.save_data(data)
+
                     click = input('Meting opgeslagen!')
                     i += 1
                 except Exception as e:
                     print(f'\n!ERROR: DATA SAVING ERROR\n{e}')
 
 
-
-    #  de 'recording_loop' is de loop die wordt re-runt voor elke enkele meting
-    # return: dB np.array()
-    def recording_loop(self):
-        pass
-        # TODO: - neem input
-        # TODO: - doe opname
-        # TODO: - verander in dB
-        # TODO: - bereken nagalmtijd
-        # TODO: - sla data op in .csv
-        return dB_recording
-
-
 # de 'microphone' class houdt alle functies die er voor nodig zijn om een opname met de microfoon te doen
 class Microphone:
     def __init__(self):
-        self.frames = 44100             # (Hz): frequentie van opnamen (standaard waarde)
-        sd.default.channels = 1    # hoeveel kanelen beschikbaar voor opname (laat 1)
-        sd.default.device = 'Microphone (USB Audio Device)'    # selecteren de juiste microfoon
-        self.test_recording_duration = 5   # hoeveel seconde lang is de test_recording
-        self.pre_recording_sleep_time = 1 # hoelang wachten tot recording start
+        self.frames = 44100                 # (Hz): frequentie van opnamen (standaard waarde)
+        sd.default.channels = 1             # hoeveel kanelen beschikbaar voor opname (laat 1)
+        sd.default.device = 'Microphone (USB Audio Device)'     # selecteren de juiste microfoon
+        self.test_recording_duration = 5    # hoeveel seconde lang is de test_recording
+        self.pre_recording_sleep_time = 1   # hoelang wachten tot recording start
         self.data_management = Data_management()
-        print(sd.query_devices())
 
 
     # doet een (rauwe) opnamen van geluid voor een bepaald aantal aangegeven secondes ('duration')
-    # return: np.array()
     def record_raw(self, duration, frames):
         raw_recording = sd.rec(int(frames * duration), samplerate=frames)
         sd.wait()
         return raw_recording
 
+
+    # prompt voor het beginnen van de opnamen, doe de opnamen en bereken de RT60
     def perform_recording(self, duration):
         self.test_recording_duration = int(duration)
         print('-'*60)
@@ -94,7 +97,7 @@ class Microphone:
         time.sleep(self.pre_recording_sleep_time)
         print('Opnemen...')
 
-        raw_recording = self.record_raw()
+        raw_recording = self.record_raw(self.test_recording_duration, self.frames)
 
         dB_recording = self.data_management.raw_recording_to_dB_recording(raw_recording)
 
@@ -103,48 +106,23 @@ class Microphone:
         mean_array, peak_array, data_message_string, RT60 = self.data_management.calculate_nagalmtijd(self.test_recording_duration, filtered_dB_recording, raw_recording)
         plot.plot_dB_and_filtered(self.test_recording_duration, dB_recording, filtered_dB_recording, mean_array, peak_array, data_message_string)
         # plot.plot_Int(self.test_recording_duration, raw_recording)
-        return RT60, raw_recording
+        return RT60
 
-
-    def fix_recording_data(self):
-
-        file_name = 'metingen_pathe_experiment.csv(1)(1)(1)'
-        data_opnames = []
-        with open(file_name, 'r') as file:
-            csv_reader = csv.reader(file)
-            data_set = []
-            for meting in csv_reader:
-                data_set.append(meting)
-        csf_titel = ['Stoel', 'Tijd', 'Nagalmtijd', 'x', 'y', 'z']
-        for meting_index in range(1, len(data_set)):
-            RT60_FOUT = float(data_set[meting_index][3])
-            RT60 = RT60_FOUT*1.2
-
-            print(f'RT60 (fout): {data_set[meting_index][3]} - RT60 (goed): {RT60}')
-            data_opnames.append([data_set[meting_index][1], data_set[meting_index][2], RT60, data_set[meting_index][5], data_set[meting_index][6], data_set[meting_index][7]])
-        file_name_save = 'gefixte_pathe_meting_3.csv'
-        with open(file_name_save, 'w', encoding='UTF8', newline='') as f:
-            csvwriter = csv.writer(f)
-            csvwriter.writerow(csf_titel)
-            csvwriter.writerows(data_opnames)
-
-
+# deze class bevat alle functies die gaan over het managen van data
 class Data_management:
     def __init__(self):
         self.file_naam = 'metingen_pathe_experiment.csv'
         while os.path.isfile('./' + self.file_naam):
             self.file_naam += '(1)'
         self.data_opnames = []
-        self.csf_titel = ['meting', 'stoel', 'tijd', 'nagalmtijd', 'raw_recording', 'x', 'y', 'z']
+        self.csf_titel = ['meting', 'stoel', 'tijd', 'nagalmtijd', 'x', 'y', 'z']
         self.nagalmtijd_diff_threschold_percentage = 5 # %
-        self.dB_mean_gain = 0
-
 
     # slaat de data van elke meting op in een csv file
     def save_data(self, new_data):
-
         # het scrhijven van nieuw lines, oftwel nieuwe data overnemen in de csv file.
         self.data_opnames.insert(len(self.data_opnames), new_data)
+
         # het schrijven van de CSV file
         with open(self.file_naam, 'w', encoding='UTF8', newline='') as f:
             csvwriter = csv.writer(f)
@@ -182,7 +160,7 @@ class Data_management:
         return filtered_dB_recording
 
 
-
+    # berekent nagalmtijd door een trendlijn te gebruiken en RT20 en RT30 te berekenen
     def nagalmtijd_function(self, nagalm_array, tijd_array):
         # functie variabelen
         tRT20 = 0
@@ -200,7 +178,7 @@ class Data_management:
         rt20_dB = max_dB - 20
         rt30_dB = max_dB - 30
 
-        # maak een linear fit van x en y
+        # maak een linear trendlijn van x en y
         try:
             popt = curve_fit(func, x, y)
         except:
@@ -228,7 +206,7 @@ class Data_management:
             RT60 = ((RT20*3)+(RT30*2))/2
             return RT60
 
-
+    # voledige functie die het berekenen van de RT60 handelt, maar ook checkt of er fouten zijn bij het berekenen
     def calculate_nagalmtijd(self, duration, filtered_dB_recording, raw_recording):
         # bereken np.arrays voor 'filtered_dB_recording' en 'raw_recording'
         x_filtered = np.linspace(0, duration, (len(np.absolute(filtered_dB_recording))))
@@ -267,8 +245,8 @@ class Data_management:
             time_frame = x_filtered[i]
             frame_array.append(dB_filtered_frame)
 
-            # bereken gemiddelde
-            mean = np.mean(np.asarray(frame_array)) + self.dB_mean_gain
+            # bereken gemiddelde van alle frames van de opname
+            mean = np.mean(np.asarray(frame_array))
 
             # bereken verschil van frame t.o.v. het gemiddelde
             difference_from_mean = (dB_filtered_frame - mean)
@@ -280,9 +258,9 @@ class Data_management:
             if dB_filtered_frame < minimum_dB:
                 minimum_dB = dB_filtered_frame
 
-            # check of er een spike is in dB en of de modus naar DETECT PEAK moet gaan
+            # check of er een spike is in dB en of de mode naar PEAK DETECTION moet gaan
             if difference_percentage >= self.nagalmtijd_diff_threschold_percentage:
-                # check of het niet het eerste frame is, negeer de eerste piek op t=0:
+                # check of frame het eerste frame is (negeer de eerste piek op t=0):
                 if time_frame == 0:
                     mode = 'NORMAL'
                     mean_list.append(mean)
@@ -312,8 +290,7 @@ class Data_management:
                 peak_counts += 1
                 counted_peak = True
                 is_peak_mode = False
-
-
+            # print informatie voor elk frame van de 'dB_recording'
             print(f''
                   f'mean: {round(mean, 2)}, '
                   f'dB: {round(dB_filtered_frame, 2)}, '
@@ -321,13 +298,13 @@ class Data_management:
                   f'diff: {round(difference_from_mean, 2)} ({round(difference_percentage, 2)}%) --- '
                   f'MODE: [{mode}]')
 
-        # check of meer dan een piek gevonden
+        # check of meer dan een piek gevonden en/of als de raw_recording 1 heeft berijkt (clipping_audio)
         if peak_counts > 1:
             warning_string += f'\nWARNING: DETECTED {peak_counts} PEAKS; MORE THEN ONE PEAK!'
         if clipping_audio:
             warning_string += f'\nWARNING: AUDIO CLIPPED AT {clipping_time}'
 
-        # print klein rapport aan het einde van de berekeningen
+        # print klein rapport aan het einde van de berekeningen en resultaten van de meting
         print('-'*60)
         print(f'COUNTED PEAKS: {peak_counts}x')
         print(f'\nMINIMUM dB: {minimum_dB}dB')
@@ -354,50 +331,25 @@ class Data_management:
         mean_array = np.asarray(mean_list)
         peak_array = np.asarray(peak_list)
         return mean_array, peak_array, data_message_string, RT60
-        # print(filtered_dB_recording)
-
-
-# de 'user_input' class houdt alle functies die er voor nodig zijn om een opname met de microfoon te doen
-class User_interface:
-    def __init__(self):
-        pass
 
 
 # de 'plot' class zorgt voor snel plotten waar dat nodig is
 class Plot:
-    def __init__(self):
-        pass
 
-    # plot dB niveau verkregen met dB_recording
-    def plot_dB(self, duration, dB_recording):
-        xpoints = np.linspace(0, duration, len(np.absolute(dB_recording)))
-        ypoints = dB_recording
-
-        plt.plot(xpoints, ypoints, label='Decibel over tijd')
-        plt.title('dB over tijd')
-        plt.ylabel('Geluidsniveau [dB]')
-        plt.xlabel('Tijd [s]')
-        plt.show()
-
-
+    # plot de gefilterde dB_recording en relevante data die daarbij wordt laten zien
     def plot_dB_and_filtered(self, duration, dB_recording, filtered_dB_recording, mean_array, peak_array, data_message_string):
         x_dB = np.linspace(0, duration, len(np.absolute(dB_recording)))
         y_dB = dB_recording
         x_filtered = np.linspace(0, duration, (len(np.absolute(filtered_dB_recording))))
         y_filtered = filtered_dB_recording
 
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-
         plt.plot(x_dB, y_dB, label='Ongefilterde dB opname', color='lightgray')
         plt.plot(x_filtered, y_filtered, label='Gefilterde dB opname', color='blue')
         plt.plot(x_filtered, mean_array, label='dB gemiddelde', color='orange')
         plt.plot(x_filtered, peak_array, '--', label='PEAK DETECTION', color='red')
         plt.plot(x_filtered, peak_array, marker='1', color='red')
-        # plt.set_facecolor("darkgray")
 
         plt.legend()
-        ax.text(30, 5, data_message_string, fontsize = 22)
         plt.title('dB tegen tijd grafiek')
         plt.ylabel('Geluidsniveau [dB] →')
         plt.xlabel('Tijd [s] →')
@@ -405,29 +357,17 @@ class Plot:
         plt.rcParams["figure.figsize"] = (60,10)
         plt.show()
 
-
-    # plot de intensiteit gemeten vanaf de microfoon
+    # plot de intensiteit gemeten vanaf de microfoon, dit is ook wel de 'raw_recording'
     def plot_Int(self, duration, raw_recording):
         xpoints = np.linspace(0, duration, len(np.absolute(raw_recording)))
         ypoints = np.absolute(raw_recording)
-
         plt.plot(xpoints, ypoints, label='I (0 tot 1)')
         plt.title('Intensiteit over tijd')
         plt.ylabel('Intensiteit [W/m2] (0 tot 1)')
         plt.xlabel('Tijd [s]')
         plt.show()
 
-#
-# file_name = 'metingen_pathe_experiment.csv'
-# with open(file_name, 'r') as file:
-#     data = csv.reader(file)
-#     for i in data:
-#         print(i)
 
-# main = Main()
-# main.main_loop()
-
-
-mic = Microphone()
-mic.fix_recording_data()
-
+# Het programma start hier, het zal in een oneidige loop gaan genaamd 'main_loop()'
+main = Main()
+main.main_loop()
